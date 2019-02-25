22bb2e72259f2884c68d6e12296e677f5e3a2b43
combet karima <k.combet@hotmail.com>
2019-02-25 11:50:19 +0100

    first commit

---
+++ resources/views/panier.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>agence de voyage</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" type="text/css" href="agence.css">
+    <link  href="http://fonts.googleapis.com/css?family=Crete+Round">
+</head>
+<body>
+<header>
+    <div id="wrapper">
+        <h1>breathe holidays<span class="orange">.</span></h1>
+        <nav>
+            <ul>
+                <li><a href="#main-image">accueil</a></li>
+                <li><a href="#steps">déstinations</a></li>
+                <li><a href="#possibilities">circuits</a></li>
+                <li><a href="#contact">contact</a></li>
+            </ul>
+
+        </nav>
+
+    </div>
+
+
+
+
+</header>
+
+<section id="main-image">
+    <div id="wrapper"></div>
+    <h2>organisez votre <br><strong>voyage sur mesur</strong></h2>
+    <a href="#" class="button-1">par ici</a>
+</section>
+
+<section  id="steps">
+    <div id="wrapper"></div>
+    <ul>
+        <li id="step1">
+            <h4>Planifier</h4>
+            <p>Confiez-nous vos rêves d'évasion : en famille ou entre amis, nous trouverons la formule qui comblera vos attentes.
+            </p>
+        </li>
+
+        <li id="step2">
+            <h4>Organisez</h4>
+            <p>
+                Bénéficiez de l'expertise de nos spécialistes de chaque destination, ils vous accompagnent dans la réalisation de votre voyage.
+            </p>
+        </li>
+
+        <li id="step3">
+            <h4>Voyagez</h4>
+            <p>
+                Nous nous chargeons d'assurer votre sécurité et de veiller à votre pleine sérénité tout au long de votre voyage.
+            </p>
+        </li>
+        <div class="clear"></div>
+    </ul>
+</section>
+
+<section id="possibilities">
+    <div id="wrapper"></div>
+    <article style="background-image: url(article.jpg);">
+        <div class="overlay">
+            <h4>Partez en famille</h4>
+            <p><small>Offrez le meilleur à ceux que vous aimez et partagez des moments fabuleux !
+                </small></p><a href="#" class="button-2">plus d'info</a>
+
+        </div>
+    </article>
+    <article style="background-image: url(desert.jpeg);">
+        <div class="overlay" >
+            <h4>Envie de s'évader</h4>
+            <p><small>Parfois un peu d'évasion serait le bienvenue et ferait le plus grand bien ! </small></p><a href="#" class="button-2">plus d'info</a>
+
+        </div>
+
+    </article>
+    <div class="clear"></div>
+
+</section>
+
+<section id="contacte">
+    <div id="wrapper"></div>
+    <h3>Contactez nous</h3>
+    <p>Chez Travel Agency nous savons que voyager est une aventure humaine mais également un engagement financier important pour vous. C'est pourquoi nous mettons un point d'honneur à prendre en compte chacune de vos attentes pour vous aider dans la préparation de votre séjour, circuit ou voyage sur mesure.</p>
+    <form></form>
+</section>
+
+
+
+
+<footer>
+
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+</body>
+</html>
